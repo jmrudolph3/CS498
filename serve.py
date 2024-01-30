487347e37911b8df2ab4aabbea3218d86748f1b0
jmrudolph3 <47836356+jmrudolph3@users.noreply.github.com>
2024-01-30 09:28:03 -0600

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import socket
+import subprocess
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def post_number():
+    args = ['python', 'stress_cpu.py']
+    subprocess.Popen(args)
+    return socket.gethostname()
+
+
+@app.route('/', methods=['GET'])
+def get_number():
+    return socket.gethostname()
+
+
+# app.add_url_rule('/', 'hello', load_balancer2)
+
+if __name__ == '__main__':
+    seed = 0
+    # app.debug = True
+    # app.host = '0.0.0.0'
+    # app.port = 80
+    app.run(host='0.0.0.0', port=5000)
